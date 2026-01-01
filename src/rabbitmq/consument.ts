e463932a7f01e28e451e5b4234a55692c1933b71
tonytor <teme4842@gmail.com>
2026-01-01 16:31:48 +0300

    feat: implement RabbitMQ connection retry and downgrade Node.js base image to 20-alpine.

--- src/rabbitmq/consument.ts
+++ src/rabbitmq/consument.ts
@@ -4,7 +4,14 @@ import amqp from 'amqplib';
 const URL = appConfig.RABBITMQ_URL;
 
 export async function connectRabbitMQChannel() {
-  const connection = await amqp.connect(URL);
-  const channel = await connection.createChannel();
-  return channel;
+  while (true) {
+    try {
+      const connection = await amqp.connect(URL);
+      const channel = await connection.createChannel();
+      return channel;
+    } catch (error) {
+      console.warn('RabbitMQ not ready, retrying in 5s...', error);
+      await new Promise((res) => setTimeout(res, 5000));
+    }
+  }
 }
