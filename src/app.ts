fbea40e1c2543a3ec7f2b08348c47b0bba9886ed
tonytor <teme4842@gmail.com>
2026-01-03 16:58:38 +0300

    refactor: Remove dotenv configuration and update post service API route to `/api/posts`.

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,15 @@
 import express from 'express';
-import dotenv from 'dotenv';
 import { connectRabbitMQChannel } from './rabbitmq/consument';
 import { startPostConsumer } from './module/post/post.consumer';
 import { logger } from './utils/logger/logger';
 import appConfig from './config/app_configs';
 import postRoutes from './module/post/post.routes';
 
-dotenv.config();
-
 async function bootstrap() {
   const app = express();
   app.use(express.json());
 
-  app.use('/posts', postRoutes);
+  app.use('/api/posts', postRoutes);
 
   const PORT = appConfig.PORT || 4002;
 
